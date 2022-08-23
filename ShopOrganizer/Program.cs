ac8bf6ab74630498dd765edd57913da24179527d
m.witzik <m.witzik@B-WRK-0224>
2022-08-23 15:45:31 +0200

    Initialisierung und Refactoring Form1 zu LoadingScreen

--- ShopOrganizer/Program.cs
+++ ShopOrganizer/Program.cs
@@ -16,7 +16,7 @@ namespace ShopOrganizer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LoadingScreen());
         }
     }
 }
