464f5ad530fc4b216037716b8438dea8b912be7c
m.witzik <m.witzik@B-WRK-0224>
2022-08-24 10:05:43 +0200

    Buttons btnProducts und btnBill erstellt optische Anpassungen der Forms

--- ShopOrganizer/MainMenuScreen.Designer.cs
+++ ShopOrganizer/MainMenuScreen.Designer.cs
@@ -29,13 +29,43 @@ namespace ShopOrganizer
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenuScreen));
+            this.btnProducts = new System.Windows.Forms.Button();
+            this.btnBill = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnProducts
+            // 
+            this.btnProducts.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnProducts.BackgroundImage")));
+            this.btnProducts.Font = new System.Drawing.Font("First Reign PERSONAL USE ONLY B", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProducts.ForeColor = System.Drawing.Color.White;
+            this.btnProducts.Location = new System.Drawing.Point(20, 20);
+            this.btnProducts.Name = "btnProducts";
+            this.btnProducts.Size = new System.Drawing.Size(275, 125);
+            this.btnProducts.TabIndex = 0;
+            this.btnProducts.Text = "Produkte verwalten";
+            this.btnProducts.UseVisualStyleBackColor = true;
+            // 
+            // btnBill
+            // 
+            this.btnBill.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnBill.BackgroundImage")));
+            this.btnBill.Font = new System.Drawing.Font("First Reign PERSONAL USE ONLY B", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBill.ForeColor = System.Drawing.Color.White;
+            this.btnBill.Location = new System.Drawing.Point(325, 20);
+            this.btnBill.Name = "btnBill";
+            this.btnBill.Size = new System.Drawing.Size(275, 125);
+            this.btnBill.TabIndex = 1;
+            this.btnBill.Text = "Rechnung erstellen";
+            this.btnBill.UseVisualStyleBackColor = true;
             // 
             // MainMenuScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(620, 165);
+            this.Controls.Add(this.btnBill);
+            this.Controls.Add(this.btnProducts);
             this.Name = "MainMenuScreen";
             this.Text = "MainMenuScreen";
             this.ResumeLayout(false);
@@ -43,5 +73,8 @@ namespace ShopOrganizer
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnProducts;
+        private System.Windows.Forms.Button btnBill;
     }
 }
